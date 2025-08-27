80ad93c052e26a3026fb80c20ce38fa3b263d759
he <he.wang@unicepta.com>
2025-08-27 09:21:26 +0200

    - added the update bookings endpoint and function

--- source/controllers/bookings.ts
+++ source/controllers/bookings.ts
@@ -34,7 +34,79 @@ const createBooking = async (req: Request, res: Response, next: NextFunction) =>
     return res.status(200).json(bookingResult);
 }
 
+const updateBooking = async (req: Request, res: Response, next: NextFunction) => {
+    const updatedBooking: Booking = req.body;
+    let existingBookings = await prisma.booking.findMany({
+        where: {
+            AND: {
+                guestName: {
+                    equals: updatedBooking.guestName,
+                },
+                unitID: {
+                    equals: updatedBooking.unitID
+                },
+            }
+        }
+    });
+    if (existingBookings.length === 0) {
+        return res.status(400).json("Booking not found");
+    }
+    if (existingBookings.length > 1) {
+        return res.status(400).json("Multiple bookings found for the same guest and unit. Please contact the developer.");
+    }
+    const existingBooking = existingBookings[0];
+
+    let isUpdatePossible = await isUpdateBookingPossible(updatedBooking, existingBooking.id);
+    if (!isUpdatePossible.result) {
+        return res.status(400).json(isUpdatePossible.reason);
+    }
+
+    let bookingResult = await prisma.booking.update({
+        where: {
+            id: existingBooking.id
+        },
+        data: {
+             checkInDate: new Date(updatedBooking.checkInDate),
+             numberOfNights: updatedBooking.numberOfNights
+       }
+    })
+
+    return res.status(200).json(bookingResult);
+}
+
 type bookingOutcome = {result:boolean, reason:string};
+
+async function isUpdateBookingPossible(updatedBooking: Booking, existingBookingId: number): Promise<bookingOutcome> {
+
+    const newCheckOutDate = new Date(updatedBooking.checkInDate);
+    newCheckOutDate.setDate(newCheckOutDate.getDate() + updatedBooking.numberOfNights);
+    // Now check if the update would overlap with other bookings
+    let overlappingBookings = await prisma.booking.findMany({
+        where: {
+            AND: {
+                checkInDate: {
+                    lt: newCheckOutDate
+                },
+                unitID: {
+                    equals: updatedBooking.unitID,
+                }
+            }
+        }
+    });
+    overlappingBookings= overlappingBookings.filter(b => {
+        if (b.id === existingBookingId) {
+            return false; // skip the existing booking
+        }
+        const bCheckOutDate = new Date(b.checkInDate);
+        bCheckOutDate.setDate(bCheckOutDate.getDate() + b.numberOfNights);
+        return bCheckOutDate > new Date(updatedBooking.checkInDate);
+    });
+    if (overlappingBookings.length > 0) {
+        return {result: false, reason: "The updated booking would overlap with existing bookings for the same unit"};
+    }
+    return {result: true, reason: "OK"};
+
+}
 
 async function isBookingPossible(booking: Booking): Promise<bookingOutcome> {
     // check 1 : The Same guest cannot book the same unit multiple times
@@ -96,4 +168,4 @@ async function isBookingPossible(booking: Booking): Promise<bookingOutcome> {
     return {result: true, reason: "OK"};
 }
 
-export default { healthCheck, createBooking }
+export default { healthCheck, createBooking, updateBooking }
--- source/routes/bookings.ts
+++ source/routes/bookings.ts
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.get('/', controller.healthCheck);
 router.post('/api/v1/booking/', controller.createBooking);
+router.patch('/api/v1/booking', controller.updateBooking);
 
 export = router;
